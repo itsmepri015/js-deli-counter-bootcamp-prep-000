c4e061dfe912eae097397d7e82307623f8033a66
naizal Edy <pritiadhikari@gmail.com>
2018-10-05 00:18:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ function currentLine(katzDeliLine){
    return "The line is currently:"+line;
    
   }
-=======
+  
+
   
     if (katzDeliLine.length === 0) {
   return "The line is currently empty."
